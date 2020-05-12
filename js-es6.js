84f093583148c7242c8462547317e4ee784040bf
ckr344 <ckr344.225@gmail.com>
2020-05-13 08:29:25 +0900

    Initial commit

---
+++ js-es6.js
@@ -0,0 +1,21 @@
+// ES6 Guide
+
+
+// ES6 vs ES2015
+
+// ES = ECMAScript
+// ECMAScript = 標準規格
+// ECMAScriptの実装がJavaScript
+// ES6 = ES2015 同じ意味
+// ES6のコード → Babelで変換 → ES5のコード = 殆どのブラウザで使えるようになる
+
+
+// 配列の便利メソッド
+// forEach
+// map
+// filter
+// find
+// every
+// some
+// reduce
+
